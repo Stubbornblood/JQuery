9887d680bfd2aac25cf6973137bcd1ed18417d09
Shivanshu Singh <ss3591419@gmail.com>
2022-05-25 13:14:00 +0530

    jQuery

---
+++ JQuerry.js
@@ -0,0 +1,69 @@
+// $("h1").click(function(){
+//      $("h1").css("color","purple");
+// // });
+
+// $("button").click(function(){
+//     $("h1").css("color","purple");
+// })
+
+// for(var i = 0;i<5;i++){
+//     document.querySelectorAll("button")[i].addEventListener("click",function(){
+//         document.querySelector("h1").style.color = "purple";
+//     });
+// }
+
+// $("button").click(function(){
+//     $("h1").css("color","purple");
+
+// })
+
+// $("input").keypress(function(event){
+//     console.log(event.key);
+// })
+
+// $("input").keypress(function(event){
+//     alert(event.key);
+// })
+
+// $("input").keypress(function(event){
+//     $("h1").text(event.key);
+// })
+
+// $("h1").on("mouseover",function(){
+//     $("h1").css("color","purple");
+// })
+ 
+// $("h1").before("<button>New</button>");
+
+// $("h1").prepend("<button>New</button>");
+
+// $("button").remove();
+
+// $("button").on("click",function(){
+//     $("h1").hide();
+// })
+
+// $("button").on("click",function(){
+//     $("h1").toggle();
+// })
+
+// $("button").on("click",function(){
+//     $("h1").fadeOut();
+// })
+
+// $("button").on("click",function(){
+//     $("h1").fadeToggle();
+// })
+
+// $("button").on("click",function(){
+//     $("h1").slideToggle();
+// })
+
+// $("button").on("click",function(){
+//     $("h1").animate({opacity:0.5}); 
+// })
+
+// $("button").on("click",function(){
+//     $("h1").slideUp().slideDown().animate({opacity : "0.5"});
+
+// })
